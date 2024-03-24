5586a561ae7eb4b11c1fc8b54378342d874764b0
kumuuuu <149645169+kumuuuu@users.noreply.github.com>
2024-03-24 12:01:36 +0530

    finalized the backend and deplied it to heroku

--- app.js
+++ app.js
@@ -2,20 +2,21 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-const mongoUrl = "mongodb+srv://User_02:test123@cluster0.diojixq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const mongoUrl = "database key";
 
-mongoose.connect(mongoUrl).then (()=>{
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
     console.log("MongoDB Connected");
-}).catch((err)=>{
-    console.log("Error",err);
+  })
+  .catch((err) => {
+    console.log("Error", err);
+  });
+
+app.get("/", (req, res) => {
+  res.send({ Status: "Started" });
 });
 
-
-
-app.get("/",(req,res)=>{
-    res.send({Status:"Started"})
-})
-
-app.listen(5001, () =>{
-    console.log("Node.js Server running on port 5001");
-})
+app.listen(5001, () => {
+  console.log("Node.js Server running on port 5001");
+});
